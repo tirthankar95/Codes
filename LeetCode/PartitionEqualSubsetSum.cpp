0ec352e0a495a2c68bfc958e72258bf70cb67477
unknown <tirthankarmittra@gmail.com>
2021-12-12 10:31:15 +0530

    12/12/2021commit

--- LeetCode/PartitionEqualSubsetSum.cpp
+++ LeetCode/PartitionEqualSubsetSum.cpp
@@ -4,20 +4,12 @@ bool dp[dim1][dim2];
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
-            sum+=nums[i];
+        int n=nums.size(),sum=0;
+        for(int i=0;i<n;i++)sum+=nums[i];
         if(sum%2)return 0;
         sum/=2;
-        for(int i=0;i<sum+1;i++)
-        {
-            for(int j=0;j<n+1;j++)
-            {
-                dp[i][j]=false;
-                if(i==0)dp[i][j]=true;
-            }//end of inner-for.
-        }//end of outer-for. 
+        memset(dp[0],true,(n+1)*sizeof(bool));
+        for(int i=1;i<=sum;i++)memset(dp[i],false,(n+1)*sizeof(bool));
         for(int i=1;i<=sum;i++)
         {
             for(int j=1;j<=n;j++)
@@ -27,8 +19,6 @@ public:
                 dp[i][j]|=dp[i][j-1];
             }//end of inner-for.
         }//end of outer-for. 
-        for(int i=1;i<=n;i++)
-            if(dp[sum][i])return true;
-        return false;
+        return dp[sum][n];
     }
 };
