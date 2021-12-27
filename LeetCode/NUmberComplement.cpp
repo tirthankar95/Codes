69d41772288f980a355e1eba58cdd96052b33e51
unknown <tirthankarmittra@gmail.com>
2021-12-27 15:13:43 +0530

    27/12/2021commit

--- LeetCode/NUmberComplement.cpp
+++ LeetCode/NUmberComplement.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int findComplement(int num) {
-        if(num==0)return 1;
-        int shift=0,ans=0;
-        while(num)
-        {
-            int bit=(bool)(num&0x1);
+        int res=0,k=0;
+        while(num){
+            res=((1^(num&1))<<k)|res;
             num>>=1;
-            ans|=(1-bit)<<shift;
-            shift++;
-        }//end of while.
-        return ans;
+            k++;
+        }
+        return res;
     }
 };
