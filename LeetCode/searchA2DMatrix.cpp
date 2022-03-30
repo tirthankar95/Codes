21a1ced41f7684175fd7aa36ae401d9e8c43ded7
unknown <tirthankarmittra@gmail.com>
2022-03-30 08:50:09 +0530

    30/03/2022commit

--- LeetCode/searchA2DMatrix.cpp
+++ LeetCode/searchA2DMatrix.cpp
@@ -1,16 +1,58 @@
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
 class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = 0, col = matrix[0].size()-1;
-        
-        while (row < matrix.size() && col >= 0) {
-            
-            if (target == matrix[row][col])
-                return true;
-            
-            target > matrix[row][col] ? row++ : col--;
+    // Returns biggest element less than target.
+    // opt -> 0 row, opt -> 1 col.
+    bool found_;
+    int binSearch(vvi& matrix,int index,int lb,int ub,int target,int opt){
+        int mid;
+        if(opt==0){
+            while(lb<=ub){
+                mid=lb+(ub-lb)/2;
+                if(matrix[index][mid]==target){
+                    found_=true;
+                    return mid;
+                }
+                if(matrix[index][mid]<target)
+                    lb=mid+1;
+                else 
+                    ub=mid-1;
+            }
+            return ub;
         }
-        
+        else{
+             while(lb<=ub){
+                mid=lb+(ub-lb)/2;
+                if(matrix[mid][index]==target){
+                    found_=true;
+                    return mid;
+                }
+                if(matrix[mid][index]<target)
+                    lb=mid+1;
+                else 
+                    ub=mid-1;
+            }
+            return ub;           
+        }
+    }
+public:
+    bool searchMatrix(vvi& matrix, int target) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int lbc=0,ubc=c-1;
+        int lbr=0,ubr=r-1;
+        found_=false;
+        while(lbc<=ubc && lbr<=ubr)
+        {
+            ubc=binSearch(matrix,lbr,lbc,ubc,target,0);
+            if(ubc==-1)return false;
+            if(found_)return true;
+            lbr=binSearch(matrix,ubc,lbr,ubr,target,1);
+            if(lbr==-1)return false;
+            if(found_)return true;
+            lbr+=1;
+        }
         return false;
     }
 };
