bcd5841cf51e14d90c97afa411a068455a32c231
tirthankar@95 <tirthankarmittra@gmail.com>
2022-06-17 10:00:08 +0530

    17/06/22Commit

--- LeetCode/LongestPalindromicSubstring.cpp
+++ LeetCode/LongestPalindromicSubstring.cpp
@@ -53,4 +53,118 @@ Can be done in O(n)
 */
 
 /*
+pproach 4: Expand Around Center
+In fact, we could solve it in O(n^2) time using only constant space.
+We observe that a palindrome mirrors around its center. Therefore, 
+a palindrome can be expanded from its center, and there are only 2n - 1 such centers.
+
+You might be asking why there are 2n - 12n−1 but not nn centers? 
+The reason is the center of a palindrome can be in between two letters. 
+Such palindromes have even number of letters (such as "abba") and its center are between the two 'b's.
+
+public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i);
+        int len2 = expandAroundCenter(s, i, i + 1);
+        int len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return s.substring(start, end + 1);
+}
+
+private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+}
+*/
+
+/*
+Manacher’s Algorithm – Linear Time Longest Palindromic Substring
+const int mxSize=3e3+1;
+int L[mxSize];
+
+string findLongestPalindromicString(string text) 
+{ 
+    int N = text.length(); 
+    if(N<=1)return text;
+    N = 2*N + 1; 
+    memset(L,0,N*sizeof(int));
+    L[0] = 0; 
+    L[1] = 1; 
+    int C = 1; //centerPosition 
+    int R = 2; //centerRightPosition 
+    int i = 0;  
+    int iMirror; //currentLeftPosition 
+    int expand = -1; 
+    int diff = -1; 
+    int maxLPSLength = 0; 
+    int maxLPSCenterPosition = 0; 
+    int start = -1; 
+    int end = -1; 
+    for (i = 2; i < N; i++) 
+    { 
+        iMirror = 2*C-i; 
+        expand = 0; 
+        diff = R - i; 
+        if(diff >= 0) 
+        { 
+            if(L[iMirror] < diff) // Case 1 
+                L[i] = L[iMirror]; 
+            else if(L[iMirror] == diff && R == N-1) // Case 2 
+                L[i] = L[iMirror]; 
+            else if(L[iMirror] == diff && R < N-1) // Case 3 
+            { 
+                    L[i] = L[iMirror]; 
+                    expand = 1; // expansion required 
+
+            } 
+            else if(L[iMirror] > diff) // Case 4 
+            { 
+                L[i] = diff; 
+                expand = 0; // expansion required 
+            } 
+        } 
+        else
+        { 
+            L[i] = 0; 
+            expand = 1; // expansion required 
+
+        } 
+        if (expand == 1) 
+        { 
+            while (((i + L[i]) < N && (i - L[i]) > 0) && ( ((i + L[i] + 1) % 2 == 0) || 
+                (text[(i + L[i] + 1)/2] == text[(i-L[i]-1)/2] ))) 
+            { 
+                L[i]++; 
+            }
+        } 
+        if(L[i] > maxLPSLength)
+        { 
+            maxLPSLength = L[i]; 
+            maxLPSCenterPosition = i; 
+        } 
+        if (i + L[i] > R) 
+        { 
+            C = i; 
+            R = i + L[i]; 
+        } 
+    } 
+    start = (maxLPSCenterPosition - maxLPSLength)/2; 
+    return text.substr(start,maxLPSLength);
+} 
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        return findLongestPalindromicString(s);
+    }
+};
 */
